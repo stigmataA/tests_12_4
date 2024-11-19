45956251533c59ad0a4c77774493748b868d0d54
stigmataA <r.muhutdinov@gmail.com>
2024-11-19 14:24:08 +0300

    Add files via upload

---
+++ tests_12_4.py
@@ -0,0 +1,47 @@
+import logging
+import unittest
+import rt_with_exceptions as runner
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @classmethod
+    def setUpClass(cls):
+        logging.basicConfig(filename='runner_tests.log', filemode='w', level=logging.INFO, encoding='utf-8',
+                            format='%(asctime)s | %(levelname)s | %(message)s')
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        try:
+            rn = runner.Runner(name='Oleg', speed=-100)
+            for _ in range(10):
+                rn.walk()
+            self.assertEqual(rn.distance, 50)
+        except ValueError:
+            logging.warning(msg='Неверная скорость для Runner')
+        logging.info('"test_walk" выполнен успешно')
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        try:
+            rn = runner.Runner(name=23, speed=10)
+            for _ in range(10):
+                rn.run()
+            self.assertEqual(rn.distance, 100)
+        except TypeError:
+            logging.warning('Неверный тип данных для объекта Runner')
+        logging.info('"test_run" выполнен успешно')
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_challenge(self):
+        rn_1 = runner.Runner(name='Oleg')
+        rn_2 = runner.Runner(name='Alex')
+        for _ in range(10):
+            rn_1.run()
+            rn_2.walk()
+        self.assertNotEqual(rn_1.distance, rn_2.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
